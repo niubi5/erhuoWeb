b3c1f41e07ed212fd7fc801fe41e92fa6c8487b9
wenwenruya <282986708@qq.com>
2016-03-30 09:45:35 +0800

    3 30

--- secondHandShop/src/com/gem/erhuo/web/SearchGoodsServlet.java
+++ secondHandShop/src/com/gem/erhuo/web/SearchGoodsServlet.java
@@ -2,6 +2,8 @@ package com.gem.erhuo.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -40,7 +42,45 @@ public class SearchGoodsServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doPost(request, response);
+		
+		
+		String word=new String(request.getParameter("word").getBytes("iso-8859-1"),"utf-8");
+		//String word=URLDecoder.decode(request.getParameter("word"), "gbk");
+		System.out.println("word:"+word);
+		int curPage = Integer.parseInt(request.getParameter("curPage"));
+		int pageSize = Integer.parseInt(request.getParameter("pageSize"));
+		
+		//获得商品集合
+		GoodService service=new GoodService();
+		UserService us = new UserService();
+		GoodsImagesService gis = new GoodsImagesService();
+		List<Goods> listGoods=service.getGoodsList(word,curPage,pageSize);
+		List<Map<Map<Goods, Users>, List<String>>> listAll = new ArrayList<Map<Map<Goods, Users>, List<String>>>();
+		// 遍历集合取出id
+	    // 通过id查找商品图片表，取出图片url，将url封装
+		for (Goods goods : listGoods) {
+			// 获得url集合
+			List<String> urls = gis.getGoodsImages(goods.getId());
+			// 通过id获得用户对象
+			Users user = us.getById(goods.getUserId());
+			Map<Map<Goods, Users>, List<String>> mapAll = new HashMap<Map<Goods, Users>, List<String>>();
+			Map<Goods, Users> goodsUsers = new HashMap<Goods, Users>();
+			// 将商品，用户加入集合
+			goodsUsers.put(goods, user);
+			// 将商品，用户，图片集合封装
+			mapAll.put(goodsUsers, urls);
+			// 加入总集合
+			listAll.add(mapAll);
+		}
+		
+		Gson gson = new GsonBuilder().enableComplexMapKeySerialization().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+		String str = gson.toJson(listAll);
+		
+		PrintWriter pw = response.getWriter();
+		pw.print(str);
+		pw.close();
+		
+		
 	}
 
 	/**
@@ -49,6 +89,7 @@ public class SearchGoodsServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//获取传来的参数
+		
 		String word=request.getParameter("word");
 		int curPage = Integer.parseInt(request.getParameter("curPage"));
 		int pageSize = Integer.parseInt(request.getParameter("pageSize"));

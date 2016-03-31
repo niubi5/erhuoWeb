f113902855f04371c1a69b4106d6a985ad262e70
chengxingen <13545293592@163.com>
2016-03-31 12:39:57 +0800

    12.39

--- secondHandShop/src/com/gem/erhuo/service/DonateService.java
+++ secondHandShop/src/com/gem/erhuo/service/DonateService.java
@@ -7,6 +7,10 @@ import com.gem.erhuo.entity.Donates;
 
 public class DonateService {
 	private DonateDao dd = new DonateDao();
+	
+	public void save(Donates donates){
+		dd.save(donates);
+	}
 
 	public List<Donates> getUserOrders(int curPage, int pageSize, int userId) {
 		return dd.getUserDonate(curPage, pageSize, userId);

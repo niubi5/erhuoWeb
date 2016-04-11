bc1799f5c1102e88a1bcbfcdf588ad2fa862e37f
heikki1993 <916310989@qq.com>
2016-04-11 13:46:15 +0800

    13.46

--- secondHandShop/src/com/gem/erhuo/dao/BaseDao.java
+++ secondHandShop/src/com/gem/erhuo/dao/BaseDao.java
@@ -9,12 +9,12 @@ public interface BaseDao<T> {
 	public void delete(int[] ids,T t) throws Exception;
 	//修改
 	public void update(T t) throws Exception;
-	//查询所有记录
+	//查询所有记录,返回一个对象集合
 	public List<T> getAll(T t) throws Exception;
-	//根据id查询
+	//根据id查询相应对象
 	public T getByID(T t,int id) throws Exception;
-	//分页查询
+	//分页查询对象，返回一个对象集合
 	public List<T> getPage(T t,int curPage,int pageSize) throws Exception;
-	//查询所有个数
+	//查询对象所有个数
 	public int getCount(T t) throws Exception;
 }
